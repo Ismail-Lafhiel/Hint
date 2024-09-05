df002355d2c73ea97c8de16b7738e13240be8fe7
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-09-05 12:13:10 +0100

    KAN-3 #done installed express, ejs and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.ejs',
+    './public/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+
