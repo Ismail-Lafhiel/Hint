6b882e9b74d309dd399701ea6177d1ad9e3892e9
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-09-12 15:48:12 +0100

    KAN-20 updated success toast after successfully registering

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -2,5 +2,14 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 exports.showLoginForm = (req, res) => {
-    res.render("login", { title: "Login", errors: [], formData: {} });
-  };
+  const successMessage = req.session.successMessage || null;
+
+  req.session.successMessage = null;
+
+  res.render("login", {
+    title: "Login",
+    errors: [],
+    formData: {},
+    successMessage,
+  });
+};
