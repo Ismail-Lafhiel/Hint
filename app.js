78aa77201babbe742b92a739fe2392cf539d5534
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-09-18 17:02:47 +0100

    Add Like Logic/Route To Comment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const syncDatabase = require("./scripts/sync"); // Import sync function
+const { deleteCommentById } = require("./controllers/commentController");
 
 const app = express();
 
@@ -20,9 +21,13 @@ syncDatabase().then(() => {
   const indexRouter = require("./routes/index");
   const registerRouter = require("./routes/register");
   const commentRouter = require("./routes/comment"); 
+  const deleteCommentRouter = require("./routes/deleteComment");
+  const likeCommentRouter = require("./routes/likeComment");
   app.use("/", indexRouter);
   app.use("/register", registerRouter);
   app.use("/comment", commentRouter);
+  app.use('/delete', deleteCommentRouter);
+  app.use('/like', likeCommentRouter);
   app.listen(3000, () => {
     console.log("Server running on port 3000");
   });
